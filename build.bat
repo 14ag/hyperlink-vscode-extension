eb79e41820be09970a933debb7cb4d1a630edbd2
14ag <muriukipn@gmail.com>
2026-01-12 04:51:03 +0300

    build script

---
+++ build.bat
@@ -0,0 +1,69 @@
+@echo off
+for %%d in (*.vsix) do set oldExtension=%%d
+if defined oldExtension (
+	move "%oldExtension%" releases
+	if errorlevel 1 (
+			echo Failed to move "%oldExtension%" to releases.
+			pause >nul
+			exit /b 1
+		)
+) else (
+	echo No .vsix file found to move.
+)
+
+set /p "patch=enter the patch number: "
+
+setlocal enabledelayedexpansion
+::dd mm yyyy 
+for /f "tokens=1-3 delims=/" %%a in ('date /t') do (
+	set dd=%%a
+	if "!dd:~0,1!"=="0" set dd=!dd:0=!
+	set mm=%%b
+	if "!mm:~0,1!"=="0" set mm=!mm:0=!
+	set "yy=%%c"
+	set "yy=!yy:~2,2!"
+	set "x=!mm!.!yy!!dd!%patch%"
+	)
+:: hh mm ss
+echo _!x!_
+pause
+for /f "tokens=1-3 delims=:" %%a in ('time /t') do (
+	set hh=%%b
+	if "!hh:~0,1!"=="0" set hh=!hh:0=!
+	set y=!hh!
+	)
+if not defined x set x=0.0
+if not defined y set y=00
+echo writing version info...
+ren "package.json" "package0.json"
+set /a "count=0"
+for /f "delims=" %%a in (package0.json) do (
+	set /a count+=1
+    set "line=%%a"
+	if !count! lss 7 (
+		echo . >nul
+		(
+		echo !line! | find /i "version" >nul
+		) && (
+			set "line=  "version": "1.!x!","
+			echo 1.!x!>version.txt
+		)
+	) 
+    echo !line!>>package.json
+)
+
+del package0.json
+cmd /c "npm run package:vsix"
+
+
+:: release_v1.!x!-build!y!.extension
+if exist extension.vsix (
+	ren "extension-release.vsix" "extension-release_v1.!x!-build!y!.vsix"
+) else (
+	echo build failed
+	endlocal
+	exit /b 1
+	)
+endlocal
+exit /b 0
+
